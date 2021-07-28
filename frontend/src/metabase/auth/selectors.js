448c0956427443b021111b043983017a9bbb0e58
alxnddr <alxnddr@gmail.com>
2021-07-28 18:00:18 +0300

    my amazing feature

--- frontend/src/metabase/auth/selectors.js
+++ frontend/src/metabase/auth/selectors.js
@@ -1,5 +1,7 @@
 import { PLUGIN_AUTH_PROVIDERS } from "metabase/plugins";
 
+console.log("as a user I want to see a console message");
+
 export function getAuthProviders(state, props) {
   return PLUGIN_AUTH_PROVIDERS.reduce(
     (providers, getProviders) => getProviders(providers, state, props),
