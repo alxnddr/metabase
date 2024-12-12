224d9b8b3d1f73310ae61fa90cdcab3254f29ecd
Aleksandr Lesnenko <alxnddr@gmail.com>
2024-12-12 18:03:44 -0500

    test change

--- frontend/src/metabase/lib/constants.js
+++ frontend/src/metabase/lib/constants.js
@@ -2,6 +2,6 @@ import { t } from "ttag";
 
 export const SEARCH_DEBOUNCE_DURATION = 300;
 
-export const DEFAULT_SEARCH_LIMIT = 50;
+export const DEFAULT_SEARCH_LIMIT = 51;
 
 export const NULL_DISPLAY_VALUE = t`(empty)`;
